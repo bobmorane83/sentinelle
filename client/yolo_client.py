ea683fea8a410c77f8576bbb0c4196a2eb80a833
Vincent Pillet <vincent.pillet@renault.com>
2024-11-14 14:48:20 +0100

    Remove file on disk

--- client/yolo_client.py
+++ client/yolo_client.py
@@ -4,11 +4,12 @@ import time
 import os
 import signal
 import sys
+from io import BytesIO
 
 # Obtenir l'URL de l'API depuis les variables d'environnement
 API_URL = os.getenv('API_URL', 'http://poc.faf.dev.gcp.renault.com:10002/upload')
 
-# Prepare Yolo NN
+# Paramètres du modèle YOLO
 Conf_threshold = 0.4
 NMS_threshold = 0.4
 COLORS = [(0, 255, 0), (0, 0, 255), (255, 0, 0), (255, 255, 0), (255, 0, 255), (0, 255, 255)]
@@ -26,7 +27,7 @@ model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
 
 starting_time = time.time()
 frame_counter = 0
-running = True  # Variable pour contrôler la boucle
+running = True
 
 def capture_image():
     global frame_counter
@@ -45,7 +46,7 @@ def capture_image():
         color = COLORS[int(classid) % len(COLORS)]
         label = "%s : %f" % (class_name[classid], score)
         cv2.rectangle(frame, box, color, 1)
-        cv2.putText(frame, label, (box[0], box[1]-10),
+        cv2.putText(frame, label, (box[0], box[1] - 10),
                    cv2.FONT_HERSHEY_COMPLEX, 0.3, color, 1)
     endingTime = time.time() - starting_time
     fps = frame_counter / endingTime
@@ -53,17 +54,17 @@ def capture_image():
                cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0), 2)
 
     if ret:
-        image_path = 'captured_image.jpg'
-        cv2.imwrite(image_path, frame)
-        return image_path
+        # Encoder l'image en mémoire au format JPEG
+        _, buffer = cv2.imencode('.jpg', frame)
+        return BytesIO(buffer)  # Retourner l'image en mémoire
     else:
         print("Erreur: Impossible de capturer l'image.")
         return None
 
-def upload_image(image_path):
-    with open(image_path, 'rb') as img:
-        files = {'image': img}
-        response = requests.post(API_URL, files=files)
+def upload_image(image_buffer):
+    # Envoyer l'image encodée directement en mémoire
+    files = {'image': ('captured_image.jpg', image_buffer, 'image/jpeg')}
+    response = requests.post(API_URL, files=files)
     print(response)
 
 def signal_handler(sig, frame):
@@ -72,13 +73,14 @@ def signal_handler(sig, frame):
     running = False
 
 if __name__ == '__main__':
-    # Associer le gestionnaire de signal pour SIGINT et SIGTERM
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
 
     while running:
-        image_path = capture_image()
-        if image_path:
-            upload_image(image_path)
+        image_buffer = capture_image()
+
+        if image_buffer:
+            # Envoyer l'image sans la sauvegarder sur disque
+            upload_image(image_buffer)
 
     print("Nettoyage des ressources et sortie.")
